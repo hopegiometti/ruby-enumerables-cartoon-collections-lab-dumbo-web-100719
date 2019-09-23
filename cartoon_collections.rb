f621330016e7f9b1ab748efdea9cd4cf2b05acf3
Hope Giometti <hopegiometti@gmail.com>
2019-09-23 19:42:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do { |type| }
+  array.find do |type|
     cheese_types.include?(type)
   end
 end
