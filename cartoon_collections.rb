253fc6cc880ea6ce060ab5b77b7aa23f26332444
Hope Giometti <hopegiometti@gmail.com>
2019-09-23 19:21:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index { |item, index| 
+    array[item] = index
+  }
 end
 
 def summon_captain_planet# code an argument here
