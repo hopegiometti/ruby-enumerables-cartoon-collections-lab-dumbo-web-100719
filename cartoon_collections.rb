7515f40b2928f4de23bb3edfdfeddceb6ba48f7a
Hope Giometti <hopegiometti@gmail.com>
2019-09-23 19:35:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,12 +11,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  i = 0 
+  i = 0
   if array.any? { |i| i.length > 4}
     return true
   else
     return false
-  i = i + 1 
+  i = i + 1
   end
 end
 
