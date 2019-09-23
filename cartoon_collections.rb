84092c85b36efc63b9e36fa13fa0ca2bb6b171c0
Hope Giometti <hopegiometti@gmail.com>
2019-09-23 19:33:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,10 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.any? { |word| word.length >= 4}
-    return true
-  else
-    return false
+  array.any? { |word| word.length >= 4}
 end
 
 def find_the_cheese# code an argument here
